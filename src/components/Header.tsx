03c97b40c93afddd597af1fb5cf0bf182107004e
Diramix <danyagranny10@proton.me>
2026-04-09 21:20:12 +0300

    style: header height refresh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,17 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.logo}>
-                <div className={styles.logoImg} />
-                <div className={styles.logoText}>Next Music</div>
+            <div className={styles.headerWrap}>
+                <div className={styles.logo}>
+                    <div className={styles.logoImg} />
+                    <div className={styles.logoText}>Next Music</div>
+                </div>
+                <nav className={styles.nav}>
+                    <Link href="/">Home</Link>
+                    <Link href="/store">Store</Link>
+                    <Link href="/fckcensor-next">FckCensor Next</Link>
+                </nav>
             </div>
-            <nav className={styles.nav}>
-                <Link href="/">Home</Link>
-                <Link href="/store">Store</Link>
-                <Link href="/fckcensor-next">FckCensor Next</Link>
-            </nav>
         </header>
     );
 }
