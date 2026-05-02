223afd87904894a3b963f1584c88aa1dae4401a2
Diramix <danyagranny10@proton.me>
2026-05-03 01:50:42 +0300

    feat: Add download button in "Next Music Client" UGC Player

--- src/components/track/TrackPageClient.tsx
+++ src/components/track/TrackPageClient.tsx
@@ -501,40 +501,33 @@ function TrackPageContent({ isHiddenMode }: { isHiddenMode: boolean }) {
 						</div>
 
 						<div className={styles.heroActions}>
-							{isHiddenMode && (
-								<button
-									onClick={() =>
-										handleDownload(
-											directUrl,
-											paramArtist,
-											paramTitle,
-											paramCover,
-										)
-									}
-									className={styles.outlineBtn}
+							<button
+								onClick={() =>
+									handleDownload(directUrl, paramArtist, paramTitle, paramCover)
+								}
+								className={styles.outlineBtn}
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									aria-hidden="true"
+									role="img"
+									width="15"
+									height="15"
+									viewBox="0 0 24 24"
 								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										aria-hidden="true"
-										role="img"
-										width="15"
-										height="15"
-										viewBox="0 0 24 24"
+									<g
+										fill="none"
+										stroke="currentColor"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
 									>
-										<g
-											fill="none"
-											stroke="currentColor"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth={2}
-										>
-											<path d="M12 15V3m9 12v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-											<path d="m7 10l5 5l5-5" />
-										</g>
-									</svg>
-									Download
-								</button>
-							)}
+										<path d="M12 15V3m9 12v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+										<path d="m7 10l5 5l5-5" />
+									</g>
+								</svg>
+								Download
+							</button>
 
 							{displayTrack?.yandexUrl && (
 								<a
